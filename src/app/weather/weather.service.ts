c0435b60fcfe6b18ae8af70f75b47cdd305463cd
Doumbia Moussa <moussa.doumbia9219@gmail.com>
2018-12-02 19:25:29 +0000

    implement feature to make e2e test work

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -4,10 +4,12 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { environment } from '../../environments/environment'
 import { ICurrentWeather, ICurrentWeatherData } from '../interfaces'
+import { IWeatherService } from './weather.service.fake'
+
 @Injectable({
   providedIn: 'root',
 })
-export class WeatherService {
+export class WeatherService implements IWeatherService {
   constructor(private httpClient: HttpClient) {}
 
   getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
@@ -23,7 +25,7 @@ export class WeatherService {
     return {
       city: data.name,
       country: data.sys.country,
-      date: data.dt * 1000,
+      date: new Date(data.dt * 1000),
       image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
       temperature: this.convertKelvinToFahrenheit(data.main.temp),
       description: data.weather[0].description,
